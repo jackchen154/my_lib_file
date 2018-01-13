19d3698dcf3af5c501e5eb1e871a10d49b0e8c71
jackchen154 <318634320@qq.com>
2018-01-13 19:31:51 +0800

    上传一个带协议的uart串口

--- 数据帧解析模板.c
+++ 数据帧解析模板.c
@@ -12,6 +12,15 @@
 #define TRUE   0
 #define Time_out 5 //串口超时时间设置
 
+//reg_name为寄存器的具体名称 
+enum reg_enum {zuolicheng=3,youlicheng,chaokuandaiX,chaokuandaiY,chaokuandai1=11,chaokuandai2,chaokuandai3,chaokuandai4,chaokuandai5,chaokuandai6,hongwai1=21,hongwai2,dianliang1=25,dianliang2};
+//reg为寄存器名称对应的实际地址 
+uchar reg[]={0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8A,0x8B,0x8C,0x8D,0x8E,0x8F,0x90,0x91,0x92,0x93,0x94,0x95,0x96,0x97,0x98,0x99,0x9A};
+//reg_buf寄存器地址所存放的数据(一个寄存器地址的数据为2字节) ,请求响应寄存器后接收解析的数据将会保存到这里 
+unsigned short reg_data[30];
+//buf为接收区的数据，接收到的原始数据将会存放到这里，然后进行解析。
+uchar receive_buf[255];
+
 
 /*******************************************************************
 * 名称：UART0_Open
@@ -169,13 +178,13 @@ int UART0_Init(int fd,int flow_ctrl,int speed,int parity,int databits,int stopbi
                      options.c_cflag |= CS7;
                      printf("7-");
                      break;
-       case 8    :    
+       case 8:    
                      options.c_cflag |= CS8;
                      printf("8-");
                      break;  
-       default   :   
-                     printf("\nData bit[%d] is not support\n",databits);
-                     return (FALSE); 
+       default:   
+                 printf("\nData bit[%d] is not support\n",databits);
+                 return (FALSE); 
     }
 
     // 设置停止位 
@@ -244,8 +253,8 @@ int UART0_Recv(int fd, uchar *rcv_buf,int data_len)
     if(fs_sele)
        {
               len = read(fd,rcv_buf,data_len);
-	          //printf("len = %d fs_sele = %d\n",len,fs_sele);
-              tcflush( fd, TCIFLUSH );//只接收data_len个数据，没接收完的部分进行清空
+	        //printf("len = %d fs_sele = %d\n",len,fs_sele);
+              tcflush( fd, TCIFLUSH );//
               return len;
        }
        else
@@ -255,137 +264,149 @@ int UART0_Recv(int fd, uchar *rcv_buf,int data_len)
        }     
 }
 
-/*******************************************************************
-* 名称：IR_learn
-* 功能：IR按键学习和学习码值发送
-* 入口参数：  fd:串口文件描述符   
-            mode:0为学习，1为学习码发送
-            num:学习码值0x00~0x64,共可以设置101个值
-                     
-* 出口参数： 0：设置成功
-           1：设置失败
-           2：接收超时(接收超时时间为5s)
-           3:串口发送失败
-           4：参数错误
-*******************************************************************/
-int IR_learn(int fd,uchar mode,uchar num)//模式设置
+unsigned short get_crc(uchar *ptr,uchar len)
 {
-  uchar learn[]={0x00,0x00,0x00,0x00,0x00};
-  uchar receive_buf=0x00,len,i;
-  if(mode==0)learn[0]=0x88;
-  if(mode==1)learn[0]=0x86;
-  if(num>=0&&num<=100)//参数判断
+  uchar i;
+  unsigned short crc=0xFFFF;
+  if(len==0) len=1;
+  while(len--)
   {
-    learn[1]=num;
-    learn[4]=learn[0]^learn[1]^learn[2]^learn[3];
-    printf("send learn data is:");  
-    for(i=0;i<5;i++)
-    printf("%x ",learn[i]);
-    printf("  ");
-
-    len = write(fd,learn,5);
-    if (len == 5 ) 
+    crc ^=*ptr;
+    for(i=0;i<8;i++)
     {
-       printf("send sucess!\n");
-       //return len;
-    }
-    else   
-    {               
-      tcflush(fd,TCOFLUSH);
-      return 3;
-    } 
- 
-    if(UART0_Recv(fd,&receive_buf,1)<0)
-    {
-       printf("接收超时！请检查模块是否连接正常。\n");
-       return 2;
-    }   
-    else
-    {
-      if(receive_buf==0x89)
-      { 
-        printf("learn成功\n"); 
-        return 0;
+      if(crc&1)
+      {
+        crc>>=1;
+        crc ^= 0XA001; 
       }
-      else if(receive_buf==0xe0)
-      { 
-        printf("learn失败\n"); 
-        return 1;
-      }
-      else printf("模块数据错误!\n");
+      else crc>>=1;
     }
-   }
-   else
-   printf("学习码值参数错误！\n");
-   
-   return 4;
+    ptr++;
+  }
+  return(crc);
 }
 
-/*******************************************************************
-* 名称：IR红外模式设置
-* 功能：将IR的模式和参数通过串口发送
-* 入口参数：  fd:串口文件描述符   
-            mode:设置的模式
-            value：设置的参数
-                     
-* 出口参数： 0：设置成功
-           1：设置失败
-           2：接收超时(接收超时时间为5s)
-           3:串口发送失败
-*******************************************************************/
-int IR_control(int fd,uchar mode,uchar value)//模式设置
-{
-  uchar control[]={0x00,0x00,0x08,0x08,0x00};
-  uchar receive_buf=0x00,len,i;
-  control[0]=mode;
-  control[1]=value;
-  control[4]=control[0]^control[1]^control[2]^control[3];
-  printf("send data is:");
-  
-  for(i=0;i<5;i++)
-  printf("%x ",control[i]);
-  printf("  ");
-  len = write(fd,control,5);
-  if (len == 5 ) 
-  {
-     printf("send sucess!\n");
-     //return len;
-  }
-  else   
-  {               
-    tcflush(fd,TCOFLUSH);
-    return 3;
-  }
- 
-    if(UART0_Recv(fd,&receive_buf,1)<0)
-    {
-       printf("接收超时！请检查模块是否连接正常。\n");
-       return 2;
-    }   
-    else
-    {
-      if(receive_buf==0x89)
-      { 
-        printf("模式设置成功\n"); 
-        return 0;
-      }
-      if(receive_buf==0xe0)
-      { 
-        printf("模式设置失败\n"); 
-        return 1;
-      }
-    }
+
+
+/*
+扫描buf中的有效数据帧并将有效数据提取保存到buf1
+*p为接收区的地址
+*p1为存放区的地址
+len为接收区的总长度 
+返回值为寄存器的个数
+*/  
+
+int get_data(int fd,uchar *p,unsigned short *p1)
+{	
+    int len;
+    uchar save_len,i;
+    unsigned short crc_result,ck_crc;
     
-    printf("模块数据错误!\n");
-    return 4;   
+    len = UART0_Recv(fd, p,200);
+    if(len>=0)
+    {
+	  //*原始数据调试输出
+	  printf("raw_len = %d\n",len);
+	  printf("the raw_data is:\n");
+      for(i=0;i<len;i++)
+   	  {
+ 	    if(receive_buf[i]>>4==0)printf("0x0%X,",receive_buf[i]);//如果数据为单数自动补上0x0
+        else 
+	    if(i==len-1)printf("0x%x}\n",receive_buf[i]);//探测是否结束
+	    else    
+        printf("0x%X,",receive_buf[i]); //正常输出  	
+      }//*/
+      while(len--)
+      {
+	     if(*p==0xaa && *(p+1)==0x55 && *(p+2)==0xcc)//帧头检测
+	     { 
+		   crc_result = get_crc(p+3,*(p+5)+3);//数据帧计算CRC
+		   ck_crc = *(p+ *(p+5)+7)<<8|*(p+*(p+5)+6);//低高字节CRC转换为整数
+		   if(crc_result==ck_crc)//CRC校验比对
+		   {   
+	          //printf("crc_ok:%d\n",crc_result);
+	          //printf("daice:%d\n",ck_crc);
+		      save_len=*(p+5)/2;//计算数据对个数
+		      //printf("save_len:%d\n",*(p+5));
+		      //printf("save_len/2:%d\n",save_len);
+	          for(i=0;i<save_len;i++)
+	          {
+	              *p1=*(p+6)<<8|*(p+6+1);//有效数据合为2个字节的数据（一个寄存器的数据值为2个字节） 
+		          //printf("%X  ",*p1);
+	              p1++;
+	              p=p+2;
+	          }
+		      break;  
+		    }
+	      }
+          p++;
+	    }
+        return save_len;
+    }
+    else
+    return FALSE;
+}
+  
 
+
+ 
+
+
+/*
+将数据进行发送
+fd为为文件描述符
+rw读写操作(0为读操作，1为写操作)
+device_n为设备地址
+reg_n为寄存器起始地址
+reg_mun为要读/写的寄存器个数
+*/
+int send_data(int fd,uchar rw,uchar device_n,uchar reg_n,uchar reg_mun)
+{
+	unsigned int crc,i,len;
+	uchar read_buf[12]={0xaa,0x55,0xcc};
+	uchar write_buf[12]={0xaa,0x55,0xcc};
+	if(rw==0)
+	{
+	  read_buf[3]=device_n;
+	  read_buf[4]=0x03;
+	  read_buf[5]=0x00;
+	  read_buf[6]=reg_n;
+	  read_buf[7]=0x00;
+	  read_buf[8]=reg_mun;
+	  crc=get_crc(&read_buf[3],6);
+	  read_buf[9]=crc&0xff;
+	  read_buf[10]=(crc>>8)&0xff;
+	  printf("\n\nsend data is:");
+	  for(i=0;i<11;i++)
+
+	  printf("%x ",read_buf[i]);
+ 	  printf("  ");
+	 
+	  len = write(fd,read_buf,11);
+          if (len == 11 ) 
+ 	  {
+             printf("send sucess!\n\n");
+             return len;
+          }
+          else   
+          {               
+            tcflush(fd,TCOFLUSH);
+            return FALSE;
+          }
+	}
 }
 
+
+
  
  
-//*
 int main(int argc, char **argv)
 {
+
+    int err;//返回调用函数的状态
+    int len;                        
+    int i;
+
     int fd=0; //文件描述符
     fd = UART0_Open(fd,"/dev/ttyS1"); //打开串口，返回文件描述符
     if(fd<0)
@@ -399,15 +420,32 @@ int main(int argc, char **argv)
        printf("UART_Init fail!\n");
        exit(1); 
     }
-
      while (1) //循环读取数据
      {  
-       IR_control(fd,0x06,0x1e);
-       //IR_learn(fd,1,10);	
-       //sleep(1);             
+       send_data(fd,0,0x81,0x80,27);                       
+       len = get_data(fd,receive_buf,reg_data);		
+       if(len > 0)
+       {
+	   //rcv_buf[len] = '\0';
+ 	   //printf("recive data is:%s\n",rcv_buf);
+       //*寄存器数据调试输出
+       printf("the reg_data len is:%d\n",len);
+	   printf("the reg_data is:\n");
+	   for(i=0;i<len;i++)
+   	   {
+ 	     if(reg_data[i]>>8==0)printf("0x000%X,",reg_data[i]);//如果数据为单数自动补上0x0
+         else 
+	     if(i==len-1)printf("0x%x}\n",reg_data[i]);//探测是否结束
+	     else    
+     	 printf("0x%X,",reg_data[i]); //正常输出  	
+        } //*/   
+	       printf("前红外左：%d  前红外右：%d\n",reg_data[hongwai1]>>8,reg_data[hongwai1]&0x00ff);
+           printf("后红外左：%d  后红外右：%d\n",reg_data[hongwai2]>>8,reg_data[hongwai2]&0x00ff);
+    
+        } 
+	    usleep(500000);                     
       }
      UART0_Close(fd);
-}//*/
-
+}
 
 
